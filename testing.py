2092bb88fbfb9cc545f8f5df3d86e8f1c1d4e28b
mrnikhil07 <nikhilkolambikar07.com>
2025-06-10 14:32:54 +0530

    added git commands

---
+++ testing.py
@@ -0,0 +1,11 @@
+print ("This is a test file.")
+def test_function():
+    print("This is a test function.")
+    return "Test successful"
+if __name__ == "__main__":
+    result = test_function()
+    print(result)
+    print("Test file executed successfully.")
+# This is a test file.
+# This is a test function.
+# Test successful 
